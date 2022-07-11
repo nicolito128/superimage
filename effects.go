534c1f566957854b27b8337a4cf80c81a3784db8
Nicolás Serna <nicolito128@hotmail.com>
2022-07-11 02:34:43 -0300

    Add Flip function

--- effects.go
+++ effects.go
@@ -29,3 +29,38 @@ func Negative(img *SuperImage) *SuperImage {
 
 	return New(inverted, img.Format)
 }
+
+// Flip inverts the image vertically returning a new *SuperImage.
+func Flip(img *SuperImage) *SuperImage {
+	bounds := img.Bounds()
+	width := img.Width
+	height := img.Height
+
+	flipped := img.Factory(&image.RGBA{}, nil).(*image.RGBA)
+	for x := bounds.Min.X; x < width; x++ {
+		for y := bounds.Min.Y; y < height/2; y++ {
+			i := flipped.PixOffset(x, y)
+			j := flipped.PixOffset(x, height-y-1)
+
+			p := flipped.Pix[i : i+4]
+			q := flipped.Pix[j : j+4]
+			r1, g1, b1, a1 := img.At(x, y).RGBA()
+			r2, g2, b2, a2 := img.At(x, height-y-1).RGBA()
+
+			c1 := color.RGBA{uint8(r1 >> 8), uint8(g1 >> 8), uint8(b1 >> 8), uint8(a1 >> 8)}
+			c2 := color.RGBA{uint8(r2 >> 8), uint8(g2 >> 8), uint8(b2 >> 8), uint8(a2 >> 8)}
+
+			p[0] = c2.R
+			p[1] = c2.G
+			p[2] = c2.B
+			p[3] = c2.A
+
+			q[0] = c1.R
+			q[1] = c1.G
+			q[2] = c1.B
+			q[3] = c1.A
+		}
+	}
+
+	return New(flipped, img.Format)
+}
